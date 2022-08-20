4486adef8cd59d4a905d2c448c2ff2db62609763
Emmanuel Diabaté <eddiabate@odc.local>
2022-08-20 04:51:06 +0000

    version 1$

---
+++ src/main/java/com/FreeTirage/Monapi/MonapiApplication.java
@@ -0,0 +1,13 @@
+package com.FreeTirage.Monapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FreeTirage/Monapi/MonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FreeTirage.Monapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
